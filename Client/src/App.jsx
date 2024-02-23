126166b06b54820203ae506fa2569f2cf3c0f3fb
Wukongthethird <simonzhang9518@gmail.com>
2024-02-22 16:26:56 -0800

    remove proxy for now. simple axios request dummy function in app

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -13,10 +13,11 @@ function App() {
   const [count, setCount] = useState(0)
   const [data, setData] = useState()
 
-  const url = "/api/v1/restaurants"
+  const url = "http://localhost:3000/api/v1/restaurants"
+  const url2 = "http://localhost:3000/api/v1/restaurants/1"
 
   function getDataFromServer(){
-    axios.get(url).then((res)=>setData(res.data)).catch((err)=>{
+    axios.get(url2).then((res)=>setData(res.data)).catch((err)=>{
       console.log(err)
     })
   }
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      "/api/v1/restaurants":"http://localhost:3000"
-    }
-  },
+  // server:{
+  //   proxy:{
+  //     "/api/v1/restaurants":"http://localhost:3000",
+      
+  //   }
+  // },
   plugins: [react()],
 })
