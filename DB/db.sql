dd3d61fa8e097657ff444cb20cfcf4a053336d27
Wukongthethird <simonzhang9518@gmail.com>
2024-02-26 22:31:37 -0800

    delete restaurants API

--- DB/db.sql
+++ DB/db.sql
@@ -29,3 +29,13 @@ CREATE TABLE
     restaurants_id INTEGER REFERENCES restaurants ON DELETE CASCADE NOT NULL,
     PRIMARY KEY (user_id,restaurants_id)
   );
+
+
+insert into yelp_users (first_name, last_name, middle_name, email, passhash) values ('Yvette', 'Mannock', 'Jere', 'sit amet lobortis sapien sapien non mi integer ac neque duis bibendum morbi', 'sed tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet');
+insert into yelp_users (first_name, last_name, middle_name, email, passhash) values ('Giorgia', 'Bellenie', 'Allix', 'iaculis diam erat fermentum justo nec condimentum neque sapien placerat ante nulla justo', 'convallis nulla neque libero convallis eget eleifend luctus ultricies eu');
+insert into yelp_users (first_name, last_name, middle_name, email, passhash) values ('Leona', 'Signore', 'Barn', 'elementum in hac habitasse platea dictumst morbi vestibulum velit id pretium iaculis diam', 'integer a nibh in quis justo maecenas rhoncus aliquam lacus morbi quis tortor id nulla ultrices aliquet maecenas leo');
+
+
+insert into user_favorites (user_id, restaurants_id) values (1,2);
+
+insert into user_favorites (first_name, last_name, middle_name, email, passhash) values ('Giorgia', 'Bellenie', 'Allix', 'iaculis diam erat fermentum justo nec condimentum neque sapien placerat ante nulla justo', 'convallis nulla neque libero convallis eget eleifend luctus ultricies eu');
